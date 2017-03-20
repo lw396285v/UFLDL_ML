c84edef50011adceae5e9ba2fd505bab7ff1ddd4
wen Li <wen Li>
2017-03-20 16:03:26 +0800

    vectorization

---
+++ starter/test.m
@@ -0,0 +1,46 @@
+% Change the filenames if you've saved the files under different names
+% On some platforms, the files might be saved as 
+% train-images.idx3-ubyte / train-labels.idx1-ubyte
+images = loadMNISTImages('train-images.idx3-ubyte');
+labels = loadMNISTLabels('train-labels.idx1-ubyte');
+ 
+% We are using display_network from the autoencoder code
+display_network(images(:,1:100)); % Show the first 100 images
+visibleSize = 28*28;   % number of input units 
+hiddenSize = 196;     % number of hidden units 
+sparsityParam = 0.1;   % desired average activation of the hidden units.
+                     % (This was denoted by the Greek alphabet rho, which looks like a lower-case "p",
+		     %  in the lecture notes). 
+lambda = 0.003;     % weight decay parameter       
+beta = 3;            % weight of sparsity penalty term       
+
+checkdisable = true; % disable gredient check.
+
+patches = images(:,1:10000);
+
+%  Obtain random parameters theta
+theta = initializeParameters(hiddenSize, visibleSize);
+
+addpath minFunc/
+options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
+                          % function. Generally, for minFunc to work, you
+                          % need a function pointer with two outputs: the
+                          % function value and the gradient. In our problem,
+                          % sparseAutoencoderCost.m satisfies this.
+options.maxIter = 400;	  % Maximum number of iterations of L-BFGS to run 
+options.display = 'on';
+
+fprintf('minFunc implementation...\n');
+[opttheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
+                                   visibleSize, hiddenSize, ...
+                                   lambda, sparsityParam, ...
+                                   beta, patches), ...
+                              theta, options);
+
+%%======================================================================
+%% STEP 5: Visualization 
+
+W1 = reshape(opttheta(1:hiddenSize*visibleSize), hiddenSize, visibleSize);
+display_network(W1', 12); 
+
+print -djpeg weights2.jpg   % save the visualization to a file 
