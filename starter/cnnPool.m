f5a3400c4400021330597fb39fff1525f791b25a
wen Li <burning-life@sjtu.edu.cn>
2017-03-24 16:44:36 +0800

    CNN final

---
+++ starter/cnnPool.m
@@ -0,0 +1,47 @@
+function pooledFeatures = cnnPool(poolDim, convolvedFeatures)
+%cnnPool Pools the given convolved features
+%
+% Parameters:
+%  poolDim - dimension of pooling region
+%  convolvedFeatures - convolved features to pool (as given by cnnConvolve)
+%                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
+%
+% Returns:
+%  pooledFeatures - matrix of pooled features in the form
+%                   pooledFeatures(featureNum, imageNum, poolRow, poolCol)
+%     
+
+numImages = size(convolvedFeatures, 2);
+numFeatures = size(convolvedFeatures, 1);
+convolvedDim = size(convolvedFeatures, 3);
+
+pooledFeatures = zeros(numFeatures, numImages, floor(convolvedDim / poolDim), floor(convolvedDim / poolDim));
+
+% -------------------- YOUR CODE HERE --------------------
+% Instructions:
+%   Now pool the convolved features in regions of poolDim x poolDim,
+%   to obtain the 
+%   numFeatures x numImages x (convolvedDim/poolDim) x (convolvedDim/poolDim) 
+%   matrix pooledFeatures, such that
+%   pooledFeatures(featureNum, imageNum, poolRow, poolCol) is the 
+%   value of the featureNum feature for the imageNum image pooled over the
+%   corresponding (poolRow, poolCol) pooling region 
+%   (see http://ufldl/wiki/index.php/Pooling )
+%   
+%   Use mean pooling here.
+% -------------------- YOUR CODE HERE --------------------
+for i = 1:numFeatures
+    for j = 1:numImages
+       poolr = 0;
+       for k = 1:poolDim:convolvedDim-poolDim+1
+          poolr = poolr + 1;
+          poolc = 0;
+          for l = 1:poolDim:convolvedDim-poolDim+1
+             poolc = poolc + 1;
+             pooledFeatures(i,j,poolr,poolc) = mean(mean(convolvedFeatures(i,j,k:k+poolDim-1,l:l+poolDim-1)));
+          end
+       end
+    end
+end
+end
+
